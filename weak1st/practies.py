5d430e946cbe34d948503af276b7e7175b30d786
Nishahtc <nishamodi254@email.com>
2024-07-13 17:57:06 +0530

    practice

---
+++ weak1st/practies.py
@@ -0,0 +1,8 @@
+square=[i**2 for i in range(1,11)]
+print(square)
+square[2]=50
+print(square)
+square.insert(5,25)
+print(square)
+square.pop()
+print(square)
---
+++ weak1st/practies2.py
@@ -0,0 +1,4 @@
+for natural_number in range (1,21):
+    print(natural_number)
+data=natural_number(-10)
+print(natural_number)
