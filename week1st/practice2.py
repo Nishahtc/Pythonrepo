80640bf770f292456b7f2ee16a5d60079751bb63
Nishahtc <nishamodi254@email.com>
2024-07-14 14:28:04 +0530

    practices2

---
+++ week1st/practice2.py
@@ -0,0 +1,11 @@
+natural_number=list(range(1,21))
+print(natural_number)
+my_list=[1,2,3,4,5,6,7,8,9,10]
+first_10=my_list[:10]
+print(first_10)
+my_list=[1,2,3,4,5,6,7,8,9,10]
+last_5=my_list[-5:]
+print(last_5)
+print("print first 10 list or last 5 list")
+print(first_10)
+print(last_5)
