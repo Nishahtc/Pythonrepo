39d88476c7c0d83d92492442364d6d9b0286096e
Nishahtc <nishamodi254@email.com>
2024-07-17 14:47:37 +0530

    practies6

---
+++ week1st/practies6.py
@@ -0,0 +1,7 @@
+mytuple=((1, 2, 3), (4, 5, 6))
+secon_element_of_first_tupple=mytuple[0][1]
+first_element_of_secondtupple=mytuple[1][0]
+print("\n Second element of first tupple : ")
+print(secon_element_of_first_tupple)
+print("\n First element of second tupple : ")
+print(first_element_of_secondtupple)
